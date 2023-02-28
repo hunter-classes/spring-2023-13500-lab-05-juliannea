ddded910df5d64a05fdfdae9f07c5e67edccc2b0
juliannea <112399053+juliannea@users.noreply.github.com>
2023-02-28 09:11:58 -0500

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,3 +1,18 @@
 #pragma once
 
 // add prototypes here
+
+
+bool isDivisibleby(int , int );
+
+bool isprime(int );
+
+int nextPrime(int );
+
+int countPrimes(int , int );
+
+bool isTwinPrime(int );
+
+int nextTwinPrime(int );
+
+int largestTwinPrime(int , int );
